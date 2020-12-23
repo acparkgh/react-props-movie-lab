7a742485d1be3906570e461038d5fd61f9897a28
acparkgh <acpark123@gmail.com>
2020-12-23 00:28:44 -0500

    completed

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,6 +6,12 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+    // console.log(movieData)
+    return (
+      movieData.map( (movie) => {return (
+                                   <MovieCard movie={movie} />
+                                )} )
+    )
   }
 
   render() {
